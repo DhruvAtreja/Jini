103d5e1f6a2bc1b151348e3d6f40ee3926f105e6
Dhruv Atreja <dhruvatrejamain@gmail.com>
2023-06-09 19:07:05 +0530

    Initial UI

--- foreground.js
+++ foreground.js
@@ -5,3 +5,39 @@
 // and injected into the same or different pages.
 
 console.log("This prints to the console of the page (injected only if the page url matched)")
+
+let chatui = document.createElement("div");
+chatui.setAttribute("id", "chatui");
+chatui.innerHTML=`
+<div id="chatheader">
+    <div id="chatheaderleft">Jini</div>
+    <div id="chatheaderright">
+        <button>Meditate</button>
+        <button>Journal</button>
+        <span id="minmaxbtn">v</span>
+    </div>
+</div>
+<div id="chatbody">
+    <div id="chatbodyleft">
+        <div id="chatbodylefttop">
+            Jini
+        </div>
+        <div id="chatbodyleftbottom">
+            Message
+        </div>
+    </div>
+    <div id="chatbodyright">
+        <div id="chatbodyrighttop">
+            You
+        </div>
+        <div id="chatbodyrightbottom">
+            Message
+        </div>
+    </div>
+</div>
+<div id="chatfooter">
+    <input type="text" placeholder="How do you feel?">
+    <button>Send</button>
+</div>
+`;
+document.querySelector("html").append(chatui);
